3051ab0edaef8f70cd040733bbda3ef89426f707
NimraAshraf32 <102245268+NimraAshraf32@users.noreply.github.com>
2022-09-08 10:19:10 +0500

    Add files via upload

---
+++ src/main/java/com/example/login_app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.login_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView username=(TextView) findViewById(R.id.username);
+        TextView password=(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbutn=(MaterialButton) findViewById(R.id.loginbuton);
+
+        //username and password
+
+        loginbutn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin")&& password.getText().toString().equals("123"))
+                {
+                    Toast.makeText(MainActivity.this,"Login Successful.",Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this,"Login Failed.",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
